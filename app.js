8dd5498b48af00dcca7b822c80c8c5620c82909e
Abir Hasan <cse.170201063@gmail.com>
2022-08-20 00:14:11 +0600

    28.7 event bubble to create calculator and clear.

--- app.js
+++ app.js
@@ -19,3 +19,22 @@ document.getElementById('generate-pin').addEventListener('click', function () {
     const pinDisplay = document.getElementById('pin-display');
     pinDisplay.value = pin;  
 })
+
+//event bubble and delegate base
+// sob button a id ba class set na kore oder parent k nia kaj ta kora hoise. tai eta delegate base
+document.getElementById('calculator').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const typedNumberField = document.getElementById('typed-number')
+    if (isNaN(number)) {
+        if (number === 'C') {
+            typedNumberField.value = '';
+        } else if (number === '<') {
+            const digits = typedNumberField.value.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typedNumberField.value = remainingDigits
+        }
+    } else {
+        typedNumberField.value += number;
+    }
+})
